fed9cb4ff8d5c47cf1cc8b9b5e20b0fc2cb1ea3e
Massimo Candela <massimo@us.ntt.net>
2022-01-23 16:49:52 +0100

    more tests about autoSave

--- tests/store.autoSave.test.js
+++ tests/store.autoSave.test.js
@@ -6,7 +6,7 @@ const expect = chai.expect;
 
 console.warn = () => {}; // Shut up
 
-describe("Store and models creation", function() {
+describe("AutoSave", function() {
 
     it("autoSave: true - 1", function (done) {
         const store = new Store({
@@ -25,13 +25,13 @@ describe("Store and models creation", function() {
                 return [{id: 1}, {id: 2}];
             },
             insert: (objects) => {
-                sets.inserted = sets.inserted.concat(objects);
+                sets.inserted = objects;
             },
             update: (objects) => {
-                sets.updated = sets.updated.concat(objects);
+                sets.updated = objects;
             },
             delete: (objects) => {
-                sets.deleted = sets.deleted.concat(objects);
+                sets.deleted = objects;
             }
         });
 
@@ -78,13 +78,13 @@ describe("Store and models creation", function() {
                 return [{id: 1}, {id: 2}];
             },
             insert: (objects) => {
-                sets.inserted = sets.inserted.concat(objects);
+                sets.inserted = objects;
             },
             update: (objects) => {
-                sets.updated = sets.updated.concat(objects);
+                sets.updated = objects;
             },
             delete: (objects) => {
-                sets.deleted = sets.deleted.concat(objects);
+                sets.deleted = objects;
             }
         });
 
@@ -97,14 +97,14 @@ describe("Store and models creation", function() {
 
                         store.insert("book", {id: 3});
                         store.find("book").then(data => {
-                            data[0].title = "ciao";
+                            data[0].title = "test";
                             data[1].destroy();
                         });
 
                         store.on("save", (status) => {
                             if (status === "end"){
                                 expect(JSON.stringify(sets.inserted)).to.equals(JSON.stringify([{id: 3}]));
-                                expect(JSON.stringify(sets.updated)).to.equals(JSON.stringify([{id: 1, title: "ciao"}]));
+                                expect(JSON.stringify(sets.updated)).to.equals(JSON.stringify([{id: 1, title: "test"}]));
                                 expect(JSON.stringify(sets.deleted)).to.equals(JSON.stringify([{id: 2}]));
                                 done();
                             }
@@ -133,13 +133,13 @@ describe("Store and models creation", function() {
                 return [{id: 1}, {id: 2}];
             },
             insert: (objects) => {
-                sets.inserted = sets.inserted.concat(objects);
+                sets.inserted = objects;
             },
             update: (objects) => {
-                sets.updated = sets.updated.concat(objects);
+                sets.updated = objects;
             },
             delete: (objects) => {
-                sets.deleted = sets.deleted.concat(objects);
+                sets.deleted = objects;
             }
         });
 
@@ -181,13 +181,13 @@ describe("Store and models creation", function() {
                 return [{id: 1}, {id: 2}];
             },
             insert: (objects) => {
-                sets.inserted = sets.inserted.concat(objects);
+                sets.inserted = objects;
             },
             update: (objects) => {
-                sets.updated = sets.updated.concat(objects);
+                sets.updated = objects;
             },
             delete: (objects) => {
-                sets.deleted = sets.deleted.concat(objects);
+                sets.deleted = objects;
             }
         });
 
@@ -235,13 +235,13 @@ describe("Store and models creation", function() {
                 return [{id: 1}, {id: 2}];
             },
             insert: (objects) => {
-                sets.inserted = sets.inserted.concat(objects);
+                sets.inserted = objects;
             },
             update: (objects) => {
-                sets.updated = sets.updated.concat(objects);
+                sets.updated = objects;
             },
             delete: (objects) => {
-                sets.deleted = sets.deleted.concat(objects);
+                sets.deleted = objects;
             }
         });
 
@@ -269,4 +269,173 @@ describe("Store and models creation", function() {
             });
     }).timeout(10000);
 
+
+    it("autoSave: false - dirty load()", function (done) {
+        const store = new Store({
+            lazyLoad: false,
+            autoSave: false
+        });
+
+        const book = new Model("book", {
+            load: () => {
+                return Promise.resolve({title: "test"});
+            },
+            retrieve: () => {
+                return [{id: 1}, {id: 2}];
+            }
+        });
+
+        store.addModel(book)
+            .then(() => {
+
+                store.find("book")
+                    .then(data => {
+                        expect(JSON.stringify(data.map(i => i.toJSON()))).to.equals(JSON.stringify([{id: 1}, {id: 2}]));
+
+                        store.on("error", (message) => {
+                            if (message === "You cannot perform load() on an unsaved object."){
+                                done();
+                            }
+                        });
+
+                        store.find("book")
+                            .then(data => {
+                                const first = data[0];
+                                first.title = "test";
+                                first.load()
+                            });
+                    });
+            });
+    }).timeout(10000);
+
+    it("autoSave: false - clean load()", function (done) {
+        const store = new Store({
+            lazyLoad: false,
+            autoSave: false
+        });
+
+        const book = new Model("book", {
+            load: () => {
+                return Promise.resolve({title: "test"});
+            },
+            retrieve: () => {
+                return [{id: 1}, {id: 2}];
+            }
+        });
+
+        store.addModel(book)
+            .then(() => {
+
+                store.find("book")
+                    .then(data => {
+                        expect(JSON.stringify(data.map(i => i.toJSON()))).to.equals(JSON.stringify([{id: 1}, {id: 2}]));
+
+                        store.on("error", (message) => {
+                            done(new Error(message));
+                        });
+
+                        store.find("book")
+                            .then(data => {
+                                const first = data[0];
+                                first.author = "author";
+                                store.save()
+                                    .then(() => {
+                                        return first.load();
+                                    })
+                                    .then(() => {
+                                        expect(JSON.stringify(first.toJSON())).to.equals(JSON.stringify({ id: 1, author: 'author', title: 'test' }));
+                                        done();
+                                    });
+                            });
+                    });
+            });
+    }).timeout(10000);
+
+    it("autoSave: true - dirty load()", function (done) {
+        const store = new Store({
+            lazyLoad: false,
+            autoSave: true
+        });
+
+        const book = new Model("book", {
+            load: () => {
+                return Promise.resolve({title: "test"});
+            },
+            retrieve: () => {
+                return [{id: 1}, {id: 2}];
+            }
+        });
+
+        store.addModel(book)
+            .then(() => {
+
+                store.find("book")
+                    .then(data => {
+                        expect(JSON.stringify(data.map(i => i.toJSON()))).to.equals(JSON.stringify([{id: 1}, {id: 2}]));
+
+                        store.on("error", (message) => {
+                            done(new Error(message));
+                        });
+
+                        store.find("book")
+                            .then(data => {
+                                const first = data[0];
+                                first.author = "author";
+                                first.load()
+                                    .then(() => {
+                                        expect(JSON.stringify(first.toJSON())).to.equals(JSON.stringify({ id: 1, author: 'author', title: 'test' }));
+                                        done();
+                                    });
+                            });
+                    });
+            });
+    }).timeout(10000);
+
+    it("autoSave: 3000", function (done) {
+        const store = new Store({
+            lazyLoad: false,
+            autoSave: 3000
+        });
+
+        let sets = {
+            inserted: [],
+            updated: [],
+            deleted: []
+        };
+
+        const book = new Model("book", {
+            retrieve: () => {
+                return [{id: 1}, {id: 2}];
+            },
+            insert: (objects) => {
+                sets.inserted = objects;
+            },
+            update: (objects) => {
+                sets.updated = objects;
+            },
+            delete: (objects) => {
+                sets.deleted = objects;
+            }
+        });
+
+        store.addModel(book)
+            .then(() => {
+                store.on("save", (status) => {
+                    if (status === "end" && sets.updated.length){
+                        expect(JSON.stringify(sets.updated)).to.equals(JSON.stringify([{id: 1, title: "ciao"}]));
+                        sets = {
+                            inserted: [],
+                            updated: [],
+                            deleted: []
+                        }
+                        done();
+                    }
+                });
+
+                store.find("book")
+                    .then(data => {
+                        data[0].title = "ciao";
+                    });
+            });
+    }).timeout(10000);
 });
